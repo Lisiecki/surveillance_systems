4879c2a4c50fc0f2b74c73121f3808c746e457fa
Dennis <dennislisiecki@gmail.com>
2016-10-03 23:56:35 +0200

    fixed drawing of rectangle

--- frame_overlay.py
+++ frame_overlay.py
@@ -10,7 +10,7 @@ from picamera.array import PiRGBArray
 from io import BytesIO
 from PIL import Image
 
-cnts = 0
+cnts = []
 
 class DetectMotion(PiMotionAnalysis):
     # Analyze motion data to determine current location of intruder
@@ -20,7 +20,10 @@ class DetectMotion(PiMotionAnalysis):
             np.square(motion_data['x'].astype(np.float)) +
             np.square(motion_data['y'].astype(np.float))
             ).clip(0, 255).astype(np.uint8)
-        (cnts, _) = cv2.findContours(motion_data, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        if (motion_data > 50).sum() > 10:
+            thresh = cv2. threshold(motion_data, 50, 255, cv2.THRESH_BINARY)[1]
+            thresh = cv2.dilate(thresh, None, iterations=2)
+            (cnts, _) = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
 with picamera.PiCamera() as camera:
     with DetectMotion(camera) as output:
@@ -40,13 +43,17 @@ with picamera.PiCamera() as camera:
 
             # loop over the contours
             for c in cnts:
-		        # if the contour is too small, ignore it
-                if cv2.contourArea(c) < 5000:
+	       	        # if the contour is too small, ignore it
+                if cv2.contourArea(c) < 100:
                     continue
  
 		        # compute the bounding box for the contour, draw it on the frame,
 		        # and update the text
                 (x, y, w, h) = cv2.boundingRect(c)
+                x *= 16
+                y *=16
+                h*=16
+                w*=16
                 cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
 	        # show the frame
             cv2.imshow("Frame", image)
